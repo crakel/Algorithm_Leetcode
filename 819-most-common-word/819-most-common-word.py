b99360b484242243c12a01d7b307788f4a55e38f
GY KIM <59333136+crakel@users.noreply.github.com>
2022-06-14 03:18:36 +0900

    Time: 58 ms (36.13%), Space: 13.9 MB (36.13%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.py
@@ -0,0 +1,25 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        dic = {}
+        
+        table = str.maketrans("!?',;.", "      ")
+        paragraph = paragraph.translate(table)
+        print(paragraph)
+# ex.translate(table)
+#         paragraph = paragraph.replace(/"!"|"?"|"'"|","|";"|"."/g, "")
+        
+        for s in paragraph.split():
+            s = s.lower()
+            if s in dic:
+                 dic[s] += 1
+            else:
+                dic[s] = 1
+        
+        for b in banned:
+            if b in dic:
+                dic.pop(b)
+        
+        max_cnt = max(dic.values())
+        for key, value in dic.items():
+            if value == max_cnt:
+                return key 
