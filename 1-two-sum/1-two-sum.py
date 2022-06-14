ab101cd9ebd95bfc6af906b9843b288c3253b578
GY KIM <59333136+crakel@users.noreply.github.com>
2022-06-14 22:16:05 +0900

    Time: 71 ms (80.43%), Space: 15.2 MB (23.81%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,17 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # 딕셔너리 활용
+        dic = {}
+        for i, num in enumerate(nums):
+            dic[num] = i
+        
+        for i, num in enumerate(nums):
+            if target - num in dic and i != dic[target-num]:
+                return [i, dic[target-num]]
+        
+        # 완전탐색 7391 ms
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
