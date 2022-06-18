854d75d1972b8a630d20193a757f12e337c469e5
GY KIM <59333136+crakel@users.noreply.github.com>
2022-06-19 03:52:26 +0900

    Time: 302 ms (79.43%), Space: 16.7 MB (79.26%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.py
+++ 561-array-partition-i/561-array-partition-i.py
@@ -1,7 +1,9 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        answer = 0
-        for i in range(0, len(nums), 2):
-            answer += nums[i]
-        return answer
+        # 슬라이싱 풀이
+        return sum(sorted(nums)[::2])
+        # nums.sort()
+        # answer = 0
+        # for i in range(0, len(nums), 2):
+        #     answer += nums[i]
+        # return answer
