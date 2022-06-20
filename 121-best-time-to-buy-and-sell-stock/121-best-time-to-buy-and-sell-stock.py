5f317b4b414b9c3ae438f1bcc21e628ad0d4d8cd
GY KIM <59333136+crakel@users.noreply.github.com>
2022-06-20 22:19:11 +0900

    Time: 2029 ms (10.56%), Space: 25 MB (37.65%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -4,12 +4,11 @@ class Solution:
         max_profit = 0
         min_price = sys.maxsize
         
-        for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            
-            max_profit = max(max_profit, prices[i] - min_price)
+        for price in prices:
+            min_price = min(min_price, price)          
+            max_profit = max(max_profit, price - min_price)
         return max_profit
+    
         # 슬라이싱 활용 -> 시간초과
         # max_profit = 0
         # for i in range(len(prices)-1):
