4b2cd1235679feae839fe403a209bbf1e6354bdc
GY KIM <59333136+crakel@users.noreply.github.com>
2022-06-20 22:10:32 +0900

    Time: 1873 ms (17.41%), Space: 25.1 MB (37.65%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,27 @@
+import sys
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = sys.maxsize
+        
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            
+            max_profit = max(max_profit, prices[i] - min_price)
+        return max_profit
+        # 슬라이싱 활용 -> 시간초과
+        # max_profit = 0
+        # for i in range(len(prices)-1):
+        #   buy = prices[i]
+        #   sell_list = prices[i+1:]
+        #   max_profit = max(max_profit, max(sell_list)-buy)
+        
+        # return max_profit
+        
+        # 브루트포스 -> 시간초과
+        # max_profit = 0
+        # for i in range(len(prices)):
+        #     for j in range(i, len(prices)):
+        #         max_profit = max(max_profit, prices[j] - prices[i])
+        # return max_profit
